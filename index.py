9b692ff6014e69a10372248fef0a5eb0fd7a123a
settime <setimetech@gmail.com>
2025-03-13 08:37:02 +0100

    version 3, added nexted while loop

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 from functions import *
-
-no_of_students = int(input('How many students do you have? '))
-i = 0
+while True:
+    try:
+        no_of_students = int(input('How many students do you have? '))
+        i = 0
+        break
+    except:
+        print('Please input a number')
 
 while i < no_of_students:
-    name = input(f'Name of student {i + 1}? ')
-    score = float(input("Score? "))
-    scores(score)
-    i = i + 1
+    while True:
+        try:
+            name = input(f'Name of student {i + 1}? ')
+            score = float(input("Score? "))
+            scores(score)
+            i = i + 1
+            break
+        except:
+            print("Invalid input")
