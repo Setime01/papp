8219eb52b618fa56a652f6047ec59217b2ec47d5
settime <setimetech@gmail.com>
2025-03-13 07:53:08 +0100

    Version 1, baisic structure

---
+++ index.py
@@ -0,0 +1,16 @@
+score = float(input("What is your score? "))
+
+if 80 <= score <= 100:
+    print("Excellent")
+
+elif 60 <= score <= 79.9:
+    print("Very Good")
+
+elif 40 <= score <= 59.9:
+    print("Good")
+
+elif 0 <= score <= 39.9:
+    print("Fair")
+
+else:
+    print('Invalid Number')
